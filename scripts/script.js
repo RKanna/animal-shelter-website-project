10f9b79be95860b344437ddf80a9c188e3f401e6
RKanna <rkannan.balakrishnan@hotmail.com>
2023-08-26 17:24:19 +0530

    dog moving code

--- scripts/script.js
+++ scripts/script.js
@@ -14,5 +14,21 @@ function carousel() {
     slideIndex = 1;
   }
   x[slideIndex - 1].style.display = "flex";
-  setTimeout(carousel, 10000); // Change image every 2 seconds
+  setTimeout(carousel, 10000);
 }
+
+const container = document.querySelector(".slider-image-container");
+const images = container.querySelectorAll("img");
+const imageWidth = images[0].clientWidth;
+let currentPosition = 0;
+let currentImageIndex = 0;
+
+function moveImages() {
+  currentPosition = -currentImageIndex * imageWidth;
+  container.style.transition = "transform 1s ease-in-out";
+  container.style.transform = `translateX(${currentPosition}px)`;
+
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+setInterval(moveImages, 1000); // Move images every 10 seconds
